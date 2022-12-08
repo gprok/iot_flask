cd4a459f9c254c98f2758c962750d24965aebfa9
gprok <gprok60@gmail.com>
2022-12-08 18:44:12 +0200

    database

--- server/server.py
+++ server/server.py
@@ -1,6 +1,21 @@
-from flask import Flask, request, jsonify 
+from flask import Flask, request, jsonify, render_template 
+import sqlite3
 
 app = Flask(__name__)
+
+conn = sqlite3.connect('data.db')
+
+print "Opened database successfully";
+
+# Create tables if they do not exist
+conn.execute('''CREATE TABLE IF NOT EXISTS sensors
+         (ID INT PRIMARY KEY     NOT NULL,
+          SENSOR_ID      INT     NOT NULL,
+          TYPE           TEXT    NOT NULL,
+          LON            REAL     NOT NULL,
+          LAT            REAL     NOT NULL,
+          VALUE          REAL     NOT NULL,
+          VALUE_DT       TEXT     NOT NULL);''')
 
 @app.route("/")
 def home():
@@ -11,3 +26,9 @@ def api_send():
     post_data = request.json
     print(post_data)
     return jsonify({'result': 'data received'})
+
+@app.route("/data")
+def data():
+    # Get all sensor data from the database
+    # Create html page to display data and pass data to the page
+    return render_template('data.html')
